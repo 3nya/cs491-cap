a56946eca8c682ecdc41f8362040e46799c06b0f
Enya Chen <enyac1207@gmail.com>
2025-02-11 00:16:04 -0600

    i can finally go home

--- classwork/cw210A.cpp
+++ classwork/cw210A.cpp
@@ -1,71 +1,87 @@
 #pragma GCC optimize("O3,unroll-loops")
 #include <vector>
 #include <iostream>
-#include <deque>
 #include <map>
 #include <set>
 using namespace std;
-typedef long long ll;
-using vl = vector<ll>;
-using vi = vector<int>;
-using vii = vector<vector<int>>;
-using pi = pair<int, int>;
 
-map<int, vi> temples;
-map<int, int> color;
+vector<vector<int>> temples;
+vector<int> color;
 vector<bool> visited;
-bool found = true;
-void dfs(map<int, vi> map, int node, int prev_color, int k) {
-    if (visited[node] || !found) {
-        return;
-    }
-    visited[node] = true;
-    vi connected = map[node];
 
-    // visited before, ensure it doesn't have the same color as curr
-    if (color.contains(node)) {
-        if (color[node] == prev_color) {
-            if (prev_color + 1 > k) {
-                if (prev_color - 1 < 0) {
-                    found = false;
-                } else {
-                    color[node]--;
+// checks all neighbors
+bool isValid(int node, int c) {
+    for (int neighbor : temples[node]) {
+        if (color[neighbor] == c) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool dfs(int node, int k) {
+    visited[node] = true;
+
+    // try all possible colors for this node
+    for (int c = 1; c <= k; c++) {
+        if (isValid(node, c)) {
+            color[node] = c;
+
+            // checks all neighbors
+            bool allNeighborsValid = true;
+            for (int neighbor : temples[node]) {
+                if (!visited[neighbor]) {
+                    if (!dfs(neighbor, k)) {
+                        allNeighborsValid = false;
+                        break;
+                    }
                 }
-            } else {
-                color[node]++;
+            }
+
+            if (allNeighborsValid) {
+                return true;
             }
         }
     }
 
 
-    for (int i = 0; i < connected.size(); i++) {
-        dfs(map, connected[i], node, k);
-    }
-
-}
-void addEdge(vector<vector<int>>& adj, int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+    return false;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-
     int n, m, k;
     cin >> n >> m >> k;
 
-    vii adj(n);
+    temples.resize(n);
+    color.resize(n, 0);
+    visited.resize(n, false);
 
     for (int i = 0; i < m; i++) {
         int x, y;
         cin >> x >> y;
-        addEdge(adj, x, y);
+        x--; y--;
+        temples[x].push_back(y);
+        temples[y].push_back(x);
     }
 
+    bool possible = true;
     for (int i = 0; i < n; i++) {
-        // dfs on the node
+        if (!visited[i]) {
+            if (!dfs(i, k)) {
+                possible = false;
+                break;
+            }
+        }
     }
 
+    if (possible) {
+        for (int i = 0; i < n; i++) {
+            cout << color[i] << "\n";
+        }
+    } else {
+        cout << -1 << "\n";
+    }
 }
