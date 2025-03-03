49ea95b15558bdcc2620fe385beefa831890aee8
Enya Chen <enyac1207@gmail.com>
2025-03-03 17:24:19 -0600

    floyd warshall with bitops

--- 2-17 all pairs shortest path/c.cpp
+++ 2-17 all pairs shortest path/c.cpp
@@ -1,23 +1,26 @@
-#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC optimize("Ofast", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
 #include <algorithm>
 #include <vector>
 #include <iostream>
-#include <unordered_map>
+#include <queue>
+#include <bitset>
 using namespace std;
-vector<vector<int>> adjlist;
+using ll = long long;
+const int MAX_N = 1000;
 
-void dfs(vector<vector<int>>& map, vector<bool>& visited, int original, int x) {
-    if (visited[x]) {
-        return;
-    }
-    visited[x] = true;
-
-    for (int neighbor : map[x]) {
-        adjlist[original][neighbor] = 1;
-        dfs(map, visited, original, neighbor);
+bitset<MAX_N> arr[MAX_N];
+void floyd_warshall(int n) {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i][k]) {  
+                arr[i] |= arr[k]; 
+            }
+        }
     }
 }
 
+// simple floyd warshall implementation to calculate all pairs shortest distance
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -25,30 +28,18 @@ int main() {
     int n;
     cin >> n;
 
-    // node, other nodes it's directed towards
-    vector<vector<int>> map(n);
-    vector<vector<int>> arr;
     for (int i = 0; i < n; i++) {
-        vector<int> temp_arr;
         for (int j = 0; j < n; j++) {
             int temp;
             cin >> temp;
-            if (temp == 1) {
-                map[i].push_back(j);
-            }
-            temp_arr.push_back(temp);
+            arr[i][j] = temp;
         }
-        arr.push_back(temp_arr);
     }
-    adjlist = arr;
-    for (int i = 0; i < n; i++) {
-        vector<bool> visited(n, false);
-        dfs(map, visited, i, i);
-    }
+    floyd_warshall(n);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << adjlist[i][j] << " ";
+            cout << arr[i][j] << " ";
         }
         cout << "\n";
     }
