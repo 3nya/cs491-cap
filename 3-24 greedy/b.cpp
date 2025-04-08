4f0e3c3a25a901de0900365ea37022ade1c32517
Enya Chen <enyac1207@gmail.com>
2025-04-07 21:57:56 -0500

    greedy

--- 3-24 greedy/b.cpp
+++ 3-24 greedy/b.cpp
@@ -25,12 +25,74 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> c[i];
     }
+
+    vi upgrades;
     for (int i = 0; i < n; i++) {
         cin >> p[i];
+        
+        if (k / p[i] + c[i] < k) {
+            upgrades.push_back(i);
+        }
     }
 
-    int coins = 0;
+    sort(upgrades.begin(), upgrades.end(), [&](int i, int j) {
+        double left = c[i] + static_cast<double>(c[j]) / p[i];
+        double right = c[j] + static_cast<double>(c[i]) / p[j];
+        return left < right;
+    });
+
     double time = 0;
+    double multipler = 1;
+    for (int i = 0; i < upgrades.size(); i++) {
+        time += c[upgrades[i]] / multipler;
+        multipler *= p[upgrades[i]];
+    }
+    
+    time += k / multipler; 
+    cout << setprecision(7) << time << "\n";
+    // for (int i = 0; i < upgrades.size(); i++) {
+    //     cout << upgrades[i] + 1 << " ";
+    // }
+    // cout << "\n";
+    // double time = 0;
+    // double mulitplier = 1;
+    // int coins = 0;
+    // for (int i = 0; i < upgrades.size(); i++) {
+    //     if (((c[upgrades[i]] - coins) / mulitplier) < ((k - coins) /  mulitplier)) {
+
+    //     } else {
+    //         break;
+    //     }
+    //     if (coins >= k) {
+    //         break;
+    //     } else {
+    //         // time to get to next upgrade, or total amount of coins (if that is less time)
+    //         // add = min((c[upgrades[i]] - coins) / mulitplier, (k - coins) /  mulitplier);
+
+    //         if (coins >= c[upgrades[i]]) {
+    //             add = (c[upgrades[i]] - coins) / mulitplier;
+    //             coins -= c[upgrades[i]];
+    //         } else {
+    //             if (((c[upgrades[i]] - coins) / mulitplier) < ((k - coins) /  mulitplier)) {
+    //                 add = c[upgrades[i]] - coins;
+    //                 coins -= c[upgrades[i]];
+
+    //                 time += add;
+    //                 coins += add * mulitplier;
+    //                 mulitplier += p[upgrades[i]];
+
+    //             } else {
+    //                 add = (k - coins) /  mulitplier;
+    //                 time += add;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    // if (coins < k) {
+    //     time += (k - coins) / mulitplier;
+    // }
+
     // k / p_i + c_i < k
 
     // time if not taking - k / p'
@@ -50,7 +112,5 @@ int main() {
     // sort based on this
     // c_i + (c_j/p_i) < c_j + (c_i/p_j)
 
-    // for (int i = 0; i < n; i++) {
-    //     if ()
-    // }
+
 }
